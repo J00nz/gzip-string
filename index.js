6052d2720862d73b67d9a6b01781afba6b9a463a
Johan Jönsson <joonz.zwy@gmail.com>
2023-10-27 09:31:48 +0200

    Made the package work.

--- index.js
+++ index.js
@@ -1,28 +1,32 @@
+const compress = async function (string, encoding = 'gzip') {
+  const byteArray = new TextEncoder().encode(string)
+  const cs = new CompressionStream(encoding)
+  const writer = cs.writable.getWriter()
+  writer.write(byteArray)
+  writer.close()
+  const buffer = await new Response(cs.readable).arrayBuffer()
+  return btoa(String.fromCharCode.apply(null, new Uint8Array(buffer)))
+}
+
+const decompress = async function(data, encoding = 'gzip') {
+  const cs = new DecompressionStream(encoding)
+  const writer = cs.writable.getWriter()
+  const byteArray = new Uint8Array([...atob(data)].map(char => char.charCodeAt(0)))
+  writer.write(byteArray)
+  writer.close()
+  const arrayBuffer = await new Response(cs.readable).arrayBuffer()
+  return new TextDecoder().decode(arrayBuffer)
+},
+
 module.exports = {
-  compress: async function (string, encoding = 'gzip') {
-    const byteArray = new TextEncoder().encode(string)
-    const cs = new CompressionStream(encoding)
-    const writer = cs.writable.getWriter()
-    writer.write(byteArray)
-    writer.close()
-    const buffer = await new Response(cs.readable).arrayBuffer()
-    return btoa(String.fromCharCode.apply(null, new Uint8Array(buffer)))
-  },
-  decompress: async function(data, encoding = 'gzip') {
-    const cs = new DecompressionStream(encoding)
-    const writer = cs.writable.getWriter()
-    const byteArray = new Uint8Array([...atob(data)].map(char => char.charCodeAt(0)))
-    writer.write(byteArray)
-    writer.close()
-    const arrayBuffer = await new Response(cs.readable).arrayBuffer()
-    return new TextDecoder().decode(arrayBuffer)
-  },
+  compress,
+  decompress,
   compressToURI: async function(string, encoding = 'gzip') {
-    const data = await this.compress(string, encoding)
+    const data = await compress(string, encoding)
     return encodeURIComponent(data)
   },
   decompressToURI: async function(string, encoding = 'gzip') {
     const data = decodeURIComponent(string)
-    return await this.decompress(data)
+    return await decompress(data)
   }
 }
